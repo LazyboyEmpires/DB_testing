bcf0c7dc4842757da05df8ff74b1e153c9933c9a
LazyboyEmpires <62291547+LazyboyEmpires@users.noreply.github.com>
2020-10-03 10:40:02 +0700

    init

---
+++ Lap02_Demo/Lap02_Demo/Form1.Designer.cs
@@ -0,0 +1,364 @@
+﻿namespace Lap02_Demo
+{
+    partial class frmTrungTam
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.cboMaHV = new System.Windows.Forms.ComboBox();
+            this.Something = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.rdNam = new System.Windows.Forms.RadioButton();
+            this.rdNu = new System.Windows.Forms.RadioButton();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtHoTen = new System.Windows.Forms.ComboBox();
+            this.dtpNgayDangKy = new System.Windows.Forms.DateTimePicker();
+            this.chkTinHocA = new System.Windows.Forms.CheckBox();
+            this.chkTinHocB = new System.Windows.Forms.CheckBox();
+            this.chkTiengAnhA = new System.Windows.Forms.CheckBox();
+            this.chkTiengAnhB = new System.Windows.Forms.CheckBox();
+            this.lblTienTHA = new System.Windows.Forms.Label();
+            this.lblTienTHB = new System.Windows.Forms.Label();
+            this.lblTienTAA = new System.Windows.Forms.Label();
+            this.lblTienTAB = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtTongTien = new System.Windows.Forms.TextBox();
+            this.btnTinhTien = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Fuchsia;
+            this.label1.Location = new System.Drawing.Point(14, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(556, 43);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "TÍNH TIỀN HỌC TRUNG TÂM";
+            // 
+            // cboMaHV
+            // 
+            this.cboMaHV.ForeColor = System.Drawing.Color.Black;
+            this.cboMaHV.FormattingEnabled = true;
+            this.cboMaHV.Location = new System.Drawing.Point(169, 64);
+            this.cboMaHV.Name = "cboMaHV";
+            this.cboMaHV.Size = new System.Drawing.Size(121, 24);
+            this.cboMaHV.TabIndex = 0;
+            // 
+            // Something
+            // 
+            this.Something.AutoSize = true;
+            this.Something.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Something.ForeColor = System.Drawing.Color.Black;
+            this.Something.Location = new System.Drawing.Point(35, 64);
+            this.Something.Name = "Something";
+            this.Something.Size = new System.Drawing.Size(119, 23);
+            this.Something.TabIndex = 2;
+            this.Something.Text = "Mã Học Viên\r\n";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(306, 64);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Giới Tính";
+            // 
+            // rdNam
+            // 
+            this.rdNam.AutoSize = true;
+            this.rdNam.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdNam.ForeColor = System.Drawing.Color.Black;
+            this.rdNam.Location = new System.Drawing.Point(386, 60);
+            this.rdNam.Name = "rdNam";
+            this.rdNam.Size = new System.Drawing.Size(58, 21);
+            this.rdNam.TabIndex = 1;
+            this.rdNam.TabStop = true;
+            this.rdNam.Text = "Nam";
+            this.rdNam.UseVisualStyleBackColor = true;
+            // 
+            // rdNu
+            // 
+            this.rdNu.AutoSize = true;
+            this.rdNu.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdNu.ForeColor = System.Drawing.Color.Black;
+            this.rdNu.Location = new System.Drawing.Point(471, 60);
+            this.rdNu.Name = "rdNu";
+            this.rdNu.Size = new System.Drawing.Size(47, 21);
+            this.rdNu.TabIndex = 2;
+            this.rdNu.TabStop = true;
+            this.rdNu.Text = "Nữ";
+            this.rdNu.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(36, 114);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 18);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Họ Tên\r\n";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Black;
+            this.label5.Location = new System.Drawing.Point(36, 160);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 17);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Ngày Đăng Ký";
+            // 
+            // txtHoTen
+            // 
+            this.txtHoTen.FormattingEnabled = true;
+            this.txtHoTen.Location = new System.Drawing.Point(169, 111);
+            this.txtHoTen.Name = "txtHoTen";
+            this.txtHoTen.Size = new System.Drawing.Size(377, 24);
+            this.txtHoTen.TabIndex = 3;
+            // 
+            // dtpNgayDangKy
+            // 
+            this.dtpNgayDangKy.Location = new System.Drawing.Point(169, 160);
+            this.dtpNgayDangKy.Name = "dtpNgayDangKy";
+            this.dtpNgayDangKy.Size = new System.Drawing.Size(275, 22);
+            this.dtpNgayDangKy.TabIndex = 4;
+            this.dtpNgayDangKy.Value = new System.DateTime(2020, 10, 16, 0, 0, 0, 0);
+            // 
+            // chkTinHocA
+            // 
+            this.chkTinHocA.AutoSize = true;
+            this.chkTinHocA.ForeColor = System.Drawing.Color.Black;
+            this.chkTinHocA.Location = new System.Drawing.Point(100, 227);
+            this.chkTinHocA.Name = "chkTinHocA";
+            this.chkTinHocA.Size = new System.Drawing.Size(90, 21);
+            this.chkTinHocA.TabIndex = 5;
+            this.chkTinHocA.Text = "Tin học A";
+            this.chkTinHocA.UseVisualStyleBackColor = true;
+            // 
+            // chkTinHocB
+            // 
+            this.chkTinHocB.AutoSize = true;
+            this.chkTinHocB.ForeColor = System.Drawing.Color.Black;
+            this.chkTinHocB.Location = new System.Drawing.Point(100, 266);
+            this.chkTinHocB.Name = "chkTinHocB";
+            this.chkTinHocB.Size = new System.Drawing.Size(90, 21);
+            this.chkTinHocB.TabIndex = 6;
+            this.chkTinHocB.Text = "Tin học B";
+            this.chkTinHocB.UseVisualStyleBackColor = true;
+            // 
+            // chkTiengAnhA
+            // 
+            this.chkTiengAnhA.AutoSize = true;
+            this.chkTiengAnhA.ForeColor = System.Drawing.Color.Black;
+            this.chkTiengAnhA.Location = new System.Drawing.Point(100, 303);
+            this.chkTiengAnhA.Name = "chkTiengAnhA";
+            this.chkTiengAnhA.Size = new System.Drawing.Size(108, 21);
+            this.chkTiengAnhA.TabIndex = 7;
+            this.chkTiengAnhA.Text = "Tiếng Anh A";
+            this.chkTiengAnhA.UseVisualStyleBackColor = true;
+            // 
+            // chkTiengAnhB
+            // 
+            this.chkTiengAnhB.AutoSize = true;
+            this.chkTiengAnhB.ForeColor = System.Drawing.Color.Black;
+            this.chkTiengAnhB.Location = new System.Drawing.Point(100, 341);
+            this.chkTiengAnhB.Name = "chkTiengAnhB";
+            this.chkTiengAnhB.Size = new System.Drawing.Size(108, 21);
+            this.chkTiengAnhB.TabIndex = 8;
+            this.chkTiengAnhB.Text = "Tiếng Anh B";
+            this.chkTiengAnhB.UseVisualStyleBackColor = true;
+            // 
+            // lblTienTHA
+            // 
+            this.lblTienTHA.AutoSize = true;
+            this.lblTienTHA.ForeColor = System.Drawing.Color.Black;
+            this.lblTienTHA.Location = new System.Drawing.Point(397, 227);
+            this.lblTienTHA.Name = "lblTienTHA";
+            this.lblTienTHA.Size = new System.Drawing.Size(96, 17);
+            this.lblTienTHA.TabIndex = 2;
+            this.lblTienTHA.Text = "300.000 đồng";
+            // 
+            // lblTienTHB
+            // 
+            this.lblTienTHB.AutoSize = true;
+            this.lblTienTHB.ForeColor = System.Drawing.Color.Black;
+            this.lblTienTHB.Location = new System.Drawing.Point(397, 270);
+            this.lblTienTHB.Name = "lblTienTHB";
+            this.lblTienTHB.Size = new System.Drawing.Size(96, 17);
+            this.lblTienTHB.TabIndex = 2;
+            this.lblTienTHB.Text = "500.000 đồng";
+            this.lblTienTHB.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // lblTienTAA
+            // 
+            this.lblTienTAA.AutoSize = true;
+            this.lblTienTAA.ForeColor = System.Drawing.Color.Black;
+            this.lblTienTAA.Location = new System.Drawing.Point(397, 307);
+            this.lblTienTAA.Name = "lblTienTAA";
+            this.lblTienTAA.Size = new System.Drawing.Size(96, 17);
+            this.lblTienTAA.TabIndex = 2;
+            this.lblTienTAA.Text = "400.000 đồng\r\n";
+            // 
+            // lblTienTAB
+            // 
+            this.lblTienTAB.AutoSize = true;
+            this.lblTienTAB.ForeColor = System.Drawing.Color.Black;
+            this.lblTienTAB.Location = new System.Drawing.Point(397, 342);
+            this.lblTienTAB.Name = "lblTienTAB";
+            this.lblTienTAB.Size = new System.Drawing.Size(96, 17);
+            this.lblTienTAB.TabIndex = 2;
+            this.lblTienTAB.Text = "600.000 đồng";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.Black;
+            this.label10.Location = new System.Drawing.Point(166, 388);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(77, 17);
+            this.label10.TabIndex = 18;
+            this.label10.Text = "Tổng tiền";
+            // 
+            // txtTongTien
+            // 
+            this.txtTongTien.Location = new System.Drawing.Point(263, 388);
+            this.txtTongTien.Name = "txtTongTien";
+            this.txtTongTien.Size = new System.Drawing.Size(150, 22);
+            this.txtTongTien.TabIndex = 9;
+            // 
+            // btnTinhTien
+            // 
+            this.btnTinhTien.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTinhTien.ForeColor = System.Drawing.Color.Blue;
+            this.btnTinhTien.Location = new System.Drawing.Point(100, 453);
+            this.btnTinhTien.Name = "btnTinhTien";
+            this.btnTinhTien.Size = new System.Drawing.Size(108, 23);
+            this.btnTinhTien.TabIndex = 11;
+            this.btnTinhTien.Text = "Tính Tiền";
+            this.btnTinhTien.UseVisualStyleBackColor = true;
+            this.btnTinhTien.Click += new System.EventHandler(this.btnTinhTien_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.ForeColor = System.Drawing.Color.Red;
+            this.btnCancel.Location = new System.Drawing.Point(263, 453);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(84, 23);
+            this.btnCancel.TabIndex = 10;
+            this.btnCancel.Text = "X Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.btnExit.Location = new System.Drawing.Point(431, 453);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 10;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // frmTrungTam
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(585, 515);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnTinhTien);
+            this.Controls.Add(this.txtTongTien);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.lblTienTAB);
+            this.Controls.Add(this.lblTienTAA);
+            this.Controls.Add(this.lblTienTHB);
+            this.Controls.Add(this.lblTienTHA);
+            this.Controls.Add(this.chkTiengAnhB);
+            this.Controls.Add(this.chkTiengAnhA);
+            this.Controls.Add(this.chkTinHocB);
+            this.Controls.Add(this.chkTinHocA);
+            this.Controls.Add(this.dtpNgayDangKy);
+            this.Controls.Add(this.txtHoTen);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.rdNu);
+            this.Controls.Add(this.rdNam);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Something);
+            this.Controls.Add(this.cboMaHV);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.Color.Magenta;
+            this.Name = "frmTrungTam";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cboMaHV;
+        private System.Windows.Forms.Label Something;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.RadioButton rdNam;
+        private System.Windows.Forms.RadioButton rdNu;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox txtHoTen;
+        private System.Windows.Forms.DateTimePicker dtpNgayDangKy;
+        private System.Windows.Forms.CheckBox chkTinHocA;
+        private System.Windows.Forms.CheckBox chkTinHocB;
+        private System.Windows.Forms.CheckBox chkTiengAnhA;
+        private System.Windows.Forms.CheckBox chkTiengAnhB;
+        private System.Windows.Forms.Label lblTienTHA;
+        private System.Windows.Forms.Label lblTienTHB;
+        private System.Windows.Forms.Label lblTienTAA;
+        private System.Windows.Forms.Label lblTienTAB;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtTongTien;
+        private System.Windows.Forms.Button btnTinhTien;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
